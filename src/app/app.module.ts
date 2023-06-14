9cb754aa5a6205c907ea8d4394d85034b858f9cc
Yadagiri Balemula <yadagiri.balemula@gpinfotech.com>
2023-06-14 11:08:44 +0530

    Yadagiri Test change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YadagiriTestComponent } from './b-yadagiri/yadagiri-test/yadagiri-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YadagiriTestComponent
   ],
   imports: [
     BrowserModule,
