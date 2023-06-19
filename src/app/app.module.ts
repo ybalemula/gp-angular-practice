43cb105ad6922112a4422079c5acd9383401376e
Sandeep Kumar G <sg179@gpinfotech.ad>
2023-06-19 18:55:46 +0530

    sandeep Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YadagiriTestComponent } from './b-yadagiri/yadagiri-test/yadagiri-test.component';
+import { SandeepTestComponent } from './g-sandeep/sandeep-test/sandeep-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    YadagiriTestComponent
+    YadagiriTestComponent,
+    SandeepTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/g-sandeep/sandeep-test/sandeep-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandeep-test',
+  templateUrl: './sandeep-test.component.html',
+  styleUrls: ['./sandeep-test.component.scss']
+})
+export class SandeepTestComponent {
+
+}
