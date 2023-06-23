b6461a892adeed078bc701e5148fb51d93490424
YBalemula <yadagiri.balemula@gpinfotech.com>
2023-06-23 16:37:23 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Navya-P/login/login.component';
 import { HomePageComponent } from './Navya-P/home-page/home-page.component';
 import { NavyaDashboardComponent } from './Navya-P/dashboard/dashboard.component';
-import { AppComponent } from './app.component';
 import { MohammadComponent } from './Mohammad/mohammad/mohammad.component';
 import { MaheshLoginComponent } from './maheshreddy/mahesh-login/mahesh-login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { YadagiriTestComponent } from './b-yadagiri/yadagiri-test/yadagiri-test.component';
-
 import { MaheshdashboardComponent } from './maheshreddy/maheshdashboard/maheshdashboard.component';
 import { MohammadDashboardComponent } from './Mohammad/mohammad/MohammadDashboard/mohammad-dashboard/mohammad-dashboard.component';
 
@@ -27,15 +25,19 @@ const routes: Routes = [
   },
   {
     path: 'mahesh',
-    component: MaheshLoginComponent,
+    component: MaheshdashboardComponent,
+  },
+  {
+    path: 'mahesh/dashboard',
+    component: MaheshdashboardComponent,
   },
   {
     path: 'mohammaddashboard',
     component: MohammadDashboardComponent,
   },
   {
-    path: 'maheshdasboard',
-    component: MaheshdashboardComponent,
+    path: 'mahesh/login',
+    component: MaheshLoginComponent,
   },
   { path: 'navya', component: NavyaDashboardComponent },
   { path: 'nvayalogin', component: LoginComponent },
